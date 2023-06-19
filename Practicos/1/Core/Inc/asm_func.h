50f4fc49d26b9c61cc33b7ac2b64fafdcfff3d90
Nicolas Cettra <nicocettra55@gmail.com>
2023-06-19 00:37:33 -0300

    Falta solamente corregir filtro, y el ejercicio 11, todo lo otro ya esta funcionando

--- Practicos/1/Core/Inc/asm_func.h
+++ Practicos/1/Core/Inc/asm_func.h
@@ -12,7 +12,7 @@ void asm_productoEscalar12(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t lon
 void asm_filtroVentana10(uint16_t *vectorIn, uint16_t *vectorOut, uint32_t longitud);
 void asm_pack32to16(int32_t *vectorIn, int16_t *vectorOut, uint32_t longitud);
 uint32_t asm_max32(int32_t *vectorIn, uint32_t longitud);
-void asm_downSampleM(int32_t *vectorIn, int32_t *vectorOut, uint32_t longitud, uint32_t N);
+void asm_downsampleM(int32_t *vectorIn, int32_t *vectorOut, uint32_t longitud, uint32_t N);
 void asm_invertir(uint16_t *vectorIn, uint32_t longitud);
-void asm_eco(int16_t *vector);
+void asm_eco(int16_t *vector,int16_t *out);
 void asm_corr(int16_t *vectorX, int16_t *vectorY, int16_t *vectorCorr, uint32_t longitud);
